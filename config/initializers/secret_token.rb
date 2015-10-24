8585baae1c16358889144976152a27f37192fa62
Mark Henderson <mrhender@uno.edu>
2015-10-24 11:44:53 -0500

    Created TodoList Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoList::Application.config.secret_key_base = '6621f7f9fdb287afa1b6e9df88f6b1d72893711768cb5642c57919186fc50224da3ba7cd41a5e7ebe1fdbceede15d1495f520005bf90e4070f92bd62988372ee'
