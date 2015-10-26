e553053b035319ff521dd7be251f09d8900fcea1
Mark Henderson <mrhender@uno.edu>
2015-10-26 10:14:07 -0500

    Adds a valdation check to make sure the description is not blank

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,5 @@
 class List < ActiveRecord::Base
   validates :title, presence: true
+  validates :title, length: {minimum: 3}
+  validates :description, presence: true
 end
--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -30,4 +30,22 @@ describe "Creating todo lists" do
     visit "/lists/"
     expect(page).to_not have_content("This is what Im doing today")
   end
+
+  it "displays an error when the todo list has a description less has no description" do
+    expect(List.count).to eq(0)
+
+    visit "/lists/"
+    click_link "New List"
+    expect(page).to have_content("New list")
+
+    fill_in "Title", with: "Grocery list"
+    fill_in "Description", with: ""
+    click_button "Create List"
+
+    expect(page).to have_content("error")
+    expect(List.count).to eq(0)
+
+    visit "/lists/"
+    expect(page).to_not have_content("Grocery list")
+  end
 end
