b11e97870ff22b913e12a33a457c36a424eeffb9
Mark Henderson <mrhender@uno.edu>
2015-10-26 10:24:02 -0500

    Refactor todo list creation in spec

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -1,28 +1,29 @@
 require 'spec_helper'
 
 describe "Creating todo lists" do
-  it "redirects to the todo list index page on success" do
+
+  def create_todo_list(options={})
+    options[:title] ||= "My todo list"
+    options[:description] ||= "This is my todo list."
+
     visit "/lists/"
     click_link "New List"
     expect(page).to have_content("New list")
 
-    fill_in "Title", with: "My todo list"
-    fill_in "Description", with: "This is what Im doing today."
+    fill_in "Title", with: options[:title]
+    fill_in "Description", with: options[:description]
     click_button "Create List"
+  end
 
+  it "redirects to the todo list index page on success" do
+    create_todo_list
     expect(page).to have_content("My todo list")
   end
 
   it "displays an error when the todo list has no title" do
     expect(List.count).to eq(0)
 
-    visit "/lists/"
-    click_link "New List"
-    expect(page).to have_content("New list")
-
-    fill_in "Title", with: ""
-    fill_in "Description", with: "This is what Im doing today."
-    click_button "Create List"
+    create_todo_list title: ""
 
     expect(page).to have_content("error")
     expect(List.count).to eq(0)
@@ -31,17 +32,11 @@ describe "Creating todo lists" do
     expect(page).to_not have_content("This is what Im doing today")
   end
 
-  it "displays an error when the todo list has a description less has no description" do
+  it "displays an error when the todo list has no description" do
     expect(List.count).to eq(0)
 
-    visit "/lists/"
-    click_link "New List"
-    expect(page).to have_content("New list")
-
-    fill_in "Title", with: "Grocery list"
-    fill_in "Description", with: ""
-    click_button "Create List"
-
+    create_todo_list description: ""
+  
     expect(page).to have_content("error")
     expect(List.count).to eq(0)
 
