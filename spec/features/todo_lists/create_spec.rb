5600ea9b54bbe1ea4781da41ebd1e1333333eee9
Mark Henderson <mrhender@uno.edu>
2015-10-25 22:46:03 -0500

    Added test

---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "Creating todo lists" do
+  it "redirects to the todo list index page on success" do
+    visit "/lists/"
+    click_link "New List"
+    expect(page).to have_content("New list")
+
+    fill_in "Title", with: "My todo list"
+    fill_in "Description", with: "This is what Im doing today."
+    click_button "Create List"
+
+    expect(page).to have_content("My todo list")
+  end
+
+  it "displays an error when the todo list has no title" do
+    expect(List.count).to eq(0)
+
+    visit "/lists/"
+    click_link "New List"
+    expect(page).to have_content("New list")
+
+    fill_in "Title", with: ""
+    fill_in "Description", with: "This is what Im doing today."
+    click_button "Create List"
+
+    expect(page).to have_content("error")
+    expect(List.count).to eq(0)
+
+    visit "/lists/"
+    expect(page).to_not have_content("This is what Im doing today")
+  end
+end
