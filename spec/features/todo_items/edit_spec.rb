151ffcf20edb26968cf4513be572c92e7ff61893
Mark Henderson <mrhender@uno.edu>
2015-10-29 12:02:07 -0500

    Added test and code for editing todo items

---
+++ spec/features/todo_items/edit_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe "Editing todo items" do
+  let!(:list) { List.create(title: "Grocery list", description: :"Groceries")}
+  let!(:item) { list.items.create(content: "Milk")}
+
+  def visit_list(list)
+    visit "/lists/"
+    within "#list_#{list.id}" do
+      click_link "List Items"
+    end
+  end
+
+  it "is successful with valid content" do
+    visit_list(list)
+    within("#item_#{item.id}") do
+      click_link "Edit"
+    end
+    fill_in "Content", with: "Lots of Milk"
+    click_button "Save"
+    expect(page).to have_content("Saved item")
+    item.reload
+    expect(item.content).to eq("Lots of Milk")
+  end
+
+  it "is unsuccessful with no content" do
+    visit_list(list)
+    within("#item_#{item.id}") do
+      click_link "Edit"
+    end
+    fill_in "Content", with: ""
+    click_button "Save"
+    expect(page).to_not have_content("Saved item")
+    expect(page).to have_content("Content can't be blank")
+    item.reload
+    expect(item.content).to eq("Milk")
+  end
+
+  it "is unsuccessful with not enough content" do
+    visit_list(list)
+    within("#item_#{item.id}") do
+      click_link "Edit"
+    end
+    fill_in "Content", with: "1"
+    click_button "Save"
+    expect(page).to_not have_content("Saved item")
+    expect(page).to have_content("Content is too short")
+    item.reload
+    expect(item.content).to eq("Milk")
+  end
+
+
+end
