748cd30436bc5359cd54c25a129b8548538ec448
Mark Henderson <mrhender@uno.edu>
2015-11-03 12:28:04 -0600

    Refactored List views

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,13 @@ class List < ActiveRecord::Base
   has_many :items
   validates :title, presence: true
   validates :description, presence: true
+
+  def has_completed_items?
+  items.complete.size > 0
+  end
+
+  def has_incomplete_items?
+  items.incomplete.size > 0
+  end
+
 end
